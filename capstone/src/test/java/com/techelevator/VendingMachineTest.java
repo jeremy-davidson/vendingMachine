c4147bbe3105c411ace2e081f71f7571ab52c8b7
Jeremy <davidson.jw@gmail.com>
2022-02-16 15:20:53 -0500

    Vending Tests

--- capstone/src/test/java/com/techelevator/VendingMachineTest.java
+++ capstone/src/test/java/com/techelevator/VendingMachineTest.java
@@ -6,34 +6,66 @@ import org.junit.Test;
 
 public class VendingMachineTest {
 
-    VendingMachine vendingMachine;
+    private VendingMachine test;
+
 
     @Before
     public void SetUp() {
-        vendingMachine = new VendingMachine();
+        this.test = new VendingMachine();
     }
 
     @Test
     public void feeding_money_feeds_balance_1_dollar() {
-        vendingMachine.feedMoney(1);
-        double actual = vendingMachine.getCurrentBalance();
+        test.feedMoney(1);
+        double actual = test.getCurrentBalance();
         Assert.assertEquals(1, actual, 0.001);
     }
 
     @Test
     public void feeding_money_feeds_balance_5_dollar() {
-        vendingMachine.feedMoney(5);
-        double actual = vendingMachine.getCurrentBalance();
+        test.feedMoney(5);
+        double actual = test.getCurrentBalance();
         Assert.assertEquals(5, actual, 0.001);
     }
 
     @Test
     public void feeding_money_feeds_balance_10_dollar() {
-        vendingMachine.feedMoney(10);
-        double actual = vendingMachine.getCurrentBalance();
+        test.feedMoney(10);
+        double actual = test.getCurrentBalance();
         Assert.assertEquals(10, actual, 0.001);
     }
 
+    @Test
+    public void returns_correct_change() {
 
+        test.currentBalance = 1.85;
+        test.returnChange();
+        double actual = test.getCurrentBalance();
 
+        Assert.assertEquals("Did not give correct change", 0.00, actual, .001);
+
+    }
+
+    @Test
+    public void reduces_inventory_when_item_is_purchased() {
+
+        test.getInventory().get("A1").purchaseItem();
+
+        int actual = test.getInventory().get("A1").getNumberInStock();
+
+        Assert.assertEquals("Did not reduce inventory when item was purchased", 4, actual);
+
+    }
+    @Test
+    public void reduces_current_balance_when_item_is_purchased() {
+
+        test.currentBalance = 5.00;
+
+        test.currentBalance = test.currentBalance - test.getInventory().get("A1").getPrice();
+
+        double actual = test.getCurrentBalance();
+
+        Assert.assertEquals("Did not reduce current balance when item was purchased", 1.95, actual, .001);
+
+    }
 }
